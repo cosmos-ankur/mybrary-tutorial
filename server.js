941615eb50651310d54acaf7db397f60a8c77286
Ankur Singh <crazyankurcool@gmail.com>
2020-11-15 23:38:09 +0530

    author routes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 const mongoose = require('mongoose')
 
 app.set('view engine','ejs')
@@ -10,6 +12,7 @@ app.set('layout','layouts/layout')
 
 
 app.use(expressLayouts)
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 app.use(express.static('public'))
 mongoose.connect('mongodb://localhost/mybrary', {useNewUrlParser: true});
 const db = mongoose.connection;
@@ -19,6 +22,7 @@ db.once('open', ()=> {
 });
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log('running at 3000')
